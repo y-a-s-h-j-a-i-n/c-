8adc8bcd42e170d3356cafe357af5285db46a357
y-a-s-h-j-a-i-n <98091626+y-a-s-h-j-a-i-n@users.noreply.github.com>
2022-03-20 21:08:54 +0530

    Create third.cpp

---
+++ file_handling_assignment/third.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    fstream myDemo;
+    string str;
+    char a[100];
+    int count=0
+    myDemo.open("demo.txt",ios::in);
+    if (!myDemo)
+    {
+        cout << "File has some error while openning !!!";
+    }
+    else
+    {    
+        while (myDemo>>str)
+        {
+                if(str.at(0) =='e'|str.at(0) =='E'){
+                    count++;
+                }
+        }
+        cout<<"word start with e are : "<<count<<"\n";  
+    }
+}
