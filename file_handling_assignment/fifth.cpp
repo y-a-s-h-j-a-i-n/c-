a1bc69c847dec111f656827600c2b11e2aa5b5dc
y-a-s-h-j-a-i-n <98091626+y-a-s-h-j-a-i-n@users.noreply.github.com>
2022-03-26 07:13:39 +0530

    Update fifth.cpp

--- file_handling_assignment/fifth.cpp
+++ file_handling_assignment/fifth.cpp
@@ -1,98 +1,110 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
 using namespace std;
-class student
+void find();
+void add();
+class Student
 {
-public:
-    int roll;
-    char name[25];
-    char branch[25];
-    char loc[25];
+     
+     int ID;
+     string name;
+     string branch;
+     string loc;
+    
+     public:
+     int getno()
+      {return ID;}
 
-    void getData()
+     void getdata()
     {
-        cout << "Enter Student ID: ";
-        cin >> roll;
-        cout<< "Enter Student Name: ";
-        cin >> name;
-        cout << "Enter Branch Name: ";
-        cin >> branch;
-        cout<< "Enter Location: ";
-        cin>> loc;
-    }
-    void addRecord()
-    {
-        fstream f;
-        student stu;
-        f.open("student.dat", ios::app | ios::binary);
-        stu.getData();
-        f.write((char *)&stu, sizeof(stu));
-        f.close();
+     cout<<"Enter ID : ";
+     cin>>ID;
+     cout<<"Enter Name     : ";
+     cin>>name;
+    
+     cout<<"Enter Branch     : ";
+     cin>>branch;
+    
+     cout<<"Enter Location     : ";
+     cin>>loc;
+
     }
     void display()
     {
-        fstream f;
-        student s;
-
-        int temp_id, flag=0;
-        cout<<"\nEnter  ID: ";
-        cin>>temp_id;
-        f.open("student.dat", ios::in | ios::binary);
-        try{
-            if(f)
-                cout<<"File Open successfully !\n";
-            else
-                throw 1; 
-        }
-        catch(int a){
-            cout<<"[EXCEPTION]: File can't opened\n";
-        }
-
-        while (f.read((char *)&s, sizeof(s)))
-        {
-            if(s.roll == temp_id){
-                cout<<"----------------------------------------------\n";
-                cout << "Student ID: " << s.roll << "\nName: " << s.name << "\nBranch: " << s.branch <<"\nLocation: "<<s.loc <<endl;
-                flag=1;
-                cout<<"----------------------------------------------\n";
-            }
-        }
-        if(flag==0){
-            cout<<"\n-----No record found-----\n\n";
-        }
-
-        f.close();
-    } 
-}; 
+     cout<<"=================================================\n";
+     cout<<"ID : "<<ID<<"\tName  : "<<name<<"\n";
+     cout<<"Branch    : "<<branch <<"\tLocation : "<<loc<<"\n";
+     
+    }
+}s;
 
 int main()
 {
-    int i=0;
-    student s1;
-    
-    while(1){
-        cout<<"----------------------------------------------\n";
-        cout<<"Press 1 student details register \nPress 2 views student details\n";
-        cout<<"Press 3 for exit\n";
-        cin>>i;
-        cout<<"----------------------------------------------\n";
-        switch (i)
-        {
-        case 1:
-            s1.addRecord();
-            break;
-        
-        case 2:
-            s1.display();
-            break;
-
-        case 3:
-            exit(1);
-
-
-        default:
-            cout<<"please select valid choices\n";
-            break;
-        }
-    }
+     int id;
+     fstream fp("student_data.txt",ios::in|ios::out|ios::binary);
+     int ch;
+     cout<<"\t1: enter Student detail\n";
+     cout<<"\t2: find Student\n";
+     cout<<"\nEnter Your Choice  : ";
+     cin>>ch;
+     while(ch)
+     {
+           switch(ch)
+           {
+                  case 1: 
+                            add(); break;
+                  case 2:
+                            find(); break;
+                  default:
+                         
+                            try
+                            {
+                                throw ch;
+                            }
+                            catch(int)
+                            {
+                                cout << "exception Caught ";
+                                cout << ch << "\n";
+                                cout<<"enter correct digit...\n"<<endl;
+                            }
+           }
+            cout<<"\t1: enter Student detail\n";
+            cout<<"\t2: find Student\n";
+            cout<<"\nEnter Your Choice  : ";
+            cin>>ch;
+     }
+}
+void find()
+{
+     ifstream fin("student_data.txt",ios::in|ios::binary);
+     int ID,i=0;
+     char found='n';
+     cout<<"\nEnter ID to be find : ";
+     cin>>ID;
+     
+     while(fin.read((char*)&s,sizeof(s)))
+     {
+          if(s.getno()==ID)
+          {
+               s.display();
+               found='y';
+          }
+          i++;
+     }
+     if(found=='n') cout<<"\nThe ID "<<ID<<" is not in the file...\n";
+     fin.close();
+}
+void add()
+{
+         char ch='y';
+         ofstream fout("student_data.txt",ios::out|ios::app|ios::binary);
+         while(ch=='y'||ch=='Y')
+         {
+              s.getdata();
+              fout.write((char*)&s,sizeof(s));
+              cout<<"\nDo you want to add more : ";
+              cin>>ch;
+         }
+         cout<<"\nData added sucessfully\n";
+         fout.close();
 }
