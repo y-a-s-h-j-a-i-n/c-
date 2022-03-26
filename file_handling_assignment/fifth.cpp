e7efd70be191f1e5aaf4b3ab7ac4d8b37532fe5b
y-a-s-h-j-a-i-n <98091626+y-a-s-h-j-a-i-n@users.noreply.github.com>
2022-03-26 14:31:15 +0530

    Update fifth.cpp

--- file_handling_assignment/fifth.cpp
+++ file_handling_assignment/fifth.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
 #include<fstream>
+#include<string.h>
 using namespace std;
 class Student
 {
-     
+     public:
      int ID;
      string name;
      string branch;
      string loc;
-
-     public:
      int getno()
      {
           return ID;
@@ -17,31 +16,46 @@ class Student
 
      void getdata()
      {
-     cout<<"Enter ID : ";
+     cout<<"Enter ID       : ";
      cin>>ID;
      cout<<"Enter Name     : ";
      cin>>name;
 
-     cout<<"Enter Branch     : ";
+     cout<<"Enter Branch   : ";
      cin>>branch;
 
-     cout<<"Enter Location     : ";
+     cout<<"Enter Location : ";
      cin>>loc;
 
      }
-     void display()
+     void display(char arr[])
      {
+          string doc[]={"ID      ","Name    ","brach   ","Location"};
+          int i=0;
      cout<<"=================================================\n";
-     cout<<"ID : "<<ID<<"\tName  : "<<name<<"\n";
-     cout<<"Branch    : "<<branch <<"\tLocation : "<<loc<<"\n";
+     for(int j=0;j<(sizeof(arr)/sizeof(arr[0]));j++)
+     {
+     if(arr[j]=='_')
+     {
+          cout<<endl;
+          cout<<doc[i];
+          cout<<" : ";
+          i+=1;
+          continue;
+     }
+     cout<<arr[j];
+     }
+     cout<<endl;
      }
 }s;
 
 int main()
 {
      int id;
+     // char id_id[20];
      fstream fp("student_data.txt",ios::in|ios::out|ios::binary);
      int ch;
+     char buf[100];
      cout<<"\t1: enter Student detail\n";
      cout<<"\t2: find Student\n";
      cout<<"\nEnter Your Choice  : ";
@@ -53,7 +67,7 @@ int main()
                while(ch=='y'||ch=='Y')
                {
                     s.getdata();
-                    fout.write((char*)&s,sizeof(s));
+                    fout<<"_"<<s.ID<<"_"<<s.name<<"_"<<s.branch<<"_"<<s.loc<<endl;
                     cout<<"\nDo you want to add more : ";
                     cin>>ch;
                }
@@ -66,15 +80,21 @@ int main()
                char found='n';
                cout<<"\nEnter ID to be find : ";
                cin>>ID;
-               
-               while(fin.read((char*)&s,sizeof(s)))
+               while(fin.getline(buf,100))
                {
-                    if(s.getno()==ID)
+                    int num=0;
+                    for(int j=1;j<(sizeof(buf)/sizeof(buf[0]));j++)
                     {
-                         s.display();
-                         found='y';
+                         if(buf[j]=='_')
+                         break;
+                         num=num*10+buf[j]-'0';
                     }
-                    i++;
+                    if(num==ID)
+                    {
+                    s.display(buf);
+                    found='y';
+                    break;
+                    }
                }
                if(found=='n') cout<<"\nThe ID "<<ID<<" is not in the file...\n";
                fin.close();
@@ -88,7 +108,7 @@ int main()
                {
                     cout << "exception Caught ";
                     cout << ch << "\n";
-                    cout<<"enter correct digit...\n"<<endl;
+                    cout<<"\nenter correct digit...\n"<<endl;
                }
           }          
           cout<<"\t1: enter Student detail\n";
